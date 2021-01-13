c570bd2245b6106f59de25011b285f48841906ad
Joan Boters <al403192@uji.es>
2021-01-13 17:25:02 +0100

    Añadimos los tests

---
+++ tests/testthat/test-regla_punto_medio.R
@@ -0,0 +1,5 @@
+test_that("punto medio works", {
+  expect_equal(regla_punto_medio(0,1,function(x){return(x)}), 0.5)
+  expect_equal(regla_punto_medio(-1,1,function(x){return(x^2)}), 0)
+  expect_equal(regla_punto_medio(-2,0,function(x){return(x+2)}), 2)
+})
